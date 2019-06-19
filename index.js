f70473b3e5f905adec38f03cfbf7457cbadba2e9
aditya063 <aditya063@yahoo.co.in>
2019-06-19 16:38:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* globals process */
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log(`App listening on port ${port}`); // eslint-disable-line no-console
+});
